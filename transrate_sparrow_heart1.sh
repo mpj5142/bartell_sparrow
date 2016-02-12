c8c424760a784bc8f4966238b588a683735763de
mpj5142 <mpj5142@psu.edu>
2016-02-12 16:33:04 -0500

    Transrate for sparrow heart tissue (sample 1 only)

---
+++ transrate_sparrow_heart1.sh
@@ -0,0 +1,3 @@
+##TransRate commands for Sparrow heart tissue
+
+transrate --assembly sparrow_data/trinity_output_heart/Trinity.fasta --left sparrow_data/Heart1Trim_R1_P.fq.gz --right sparrow_data/Heart1Trim_R2_P.fq.gz --threads 8
