c3efc101a5caad2b25c9ece00808d0322afed94c
mpj5142 <mpj5142@psu.edu>
2016-02-15 12:48:09 -0500

    Script to run Transrate on concatanated .fastq read files for sparrow project

---
+++ transrate_sparrow_heart_all.sh
@@ -0,0 +1,5 @@
+##TransRate commands for Sparrow heart tissue
+
+#!/bin/bash
+
+transrate --assembly sparrow_data/trinity_output_heart/Trinity.fasta --left HeartTrim_All_R1_P.fq.gz --right HeartTrim_All_R2_P.fq.gz --threads 8
